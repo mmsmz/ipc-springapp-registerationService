75f60c2af2feb79e8a37d5a1bb9ee6626b677ad6
mohamed <marafath07@gmail.com>
2020-11-12 01:14:47 +0530

    added active origin in controller

--- src/main/java/com/Ipjpro/RegistrationService/Controller/HomeController.java
+++ src/main/java/com/Ipjpro/RegistrationService/Controller/HomeController.java
@@ -3,13 +3,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.Ipjpro.RegistrationService.Dto.StudentDto;
 import com.Ipjpro.RegistrationService.Service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HomeController {
 	
 	@Autowired
